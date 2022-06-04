cc7a87c96ce54d65f409cc7eb0a9c6be1e8eed2f
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-06-04 03:19:15 -0700

    new light mode blue gradient

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,6 +1,6 @@
 export const lightTheme = {
   textColor: "black",
-  backgroundColor: "grey"
+  backgroundColor: "linear-gradient(90deg, rgba(14,11,69,1) 0%, rgba(19,35,150,1) 43%, rgba(31,179,209,1) 87%);"
   
   };
   
