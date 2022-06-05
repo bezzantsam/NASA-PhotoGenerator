dc9423b737b2151e5ea511d21a47364fc327ef60
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-06-05 00:56:33 -0700

    changed light theme background to NASA blue

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,7 +1,7 @@
 export const lightTheme = {
   textColor: "black",
   backgroundColor:
-    "linear-gradient(90deg, rgba(14,11,69,1) 0%, rgba(19,35,150,1) 43%, rgba(31,179,209,1) 87%);",
+  "#105bd8;",
 };
 
 export const darkTheme = {
